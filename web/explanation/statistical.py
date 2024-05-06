ce88ec3f95005b47cf8cbbaf2aeaedab6e80fd33
Danyil <mail.ordan@gmail.com>
2024-05-07 00:46:34 +0300

    Enhance NLE jupyter notebook with most probable operators and dimensions to be applied

--- web/explanation/statistical.py
+++ web/explanation/statistical.py
@@ -40,7 +40,7 @@ def length(payloads):
     return int(max_length)
 
 
-def dist_operators(payloads):
+def dist_operators(payloads, top_k: int = 2):
     operators = ['remaining_operators_by_neighbors', 'remaining_operators_by_superset', 'remaining_operators_by_distribution', 'remaining_operators_by_facet']
     operator_values = defaultdict(float)
 
@@ -50,13 +50,13 @@ def dist_operators(payloads):
     total_count = sum(operator_values.values()) + 0.000001
     percentages_length = {key: value / total_count * 100 for key, value in operator_values.items()}
 
-    top_two_values = heapq.nlargest(4, percentages_length.values())
-    top_two_keys = [(key, value) for key, value in percentages_length.items() if value in top_two_values]
+    top_k_values = heapq.nlargest(top_k, percentages_length.values())
+    top_k_keys = [(key, value) for key, value in percentages_length.items() if value in top_k_values]
 
-    return top_two_keys
+    return top_k_keys
 
 
-def dimensions(payloads):
+def dist_dimensions(payloads, top_k: int = 2):
     dimensions = ['remaining_dimensions_u', 'remaining_dimensions_g', 'remaining_dimensions_r', 'remaining_dimensions_i', 'remaining_dimensions_z', 'remaining_dimensions_petroRad_r', 'remaining_dimensions_redshift']
     dimensions_values = defaultdict(float)
 
@@ -66,10 +66,10 @@ def dimensions(payloads):
     total_count = sum(dimensions_values.values()) + 0.000001
     percentages_length = {key: value / total_count * 100 for key, value in dimensions_values.items()}
 
-    top_two_values = heapq.nlargest(2, percentages_length.values())
-    top_two_keys = [(key, value) for key, value in percentages_length.items() if value in top_two_values]
+    top_k_values = heapq.nlargest(top_k, percentages_length.values())
+    top_k_keys = [(key, value) for key, value in percentages_length.items() if value in top_k_values]
 
-    return top_two_keys
+    return top_k_keys
 
 
 def familiarity(payloads):
@@ -104,7 +104,7 @@ def scattered_or_concentrated(payloads):
 def generate_guidance(payloads, step):
     total_length = length(payloads)
     operator = dist_operators(payloads)
-    dimension = dimensions(payloads)
+    dimension = dist_dimensions(payloads)
     median_familiarity = familiarity(payloads)
     k = number_of_sim_pipelines(payloads)
     target_type = scattered_or_concentrated(payloads)
